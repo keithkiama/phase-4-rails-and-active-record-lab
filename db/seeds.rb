dcde2a94e58e29b99962cfcb6eb78d11554e9063
keithkiama <keithkiama2003@gmail.com>
2023-03-13 23:14:06 +0300

    Completed Assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "seeding"
+therock = Student.create([{first_name: "Dwayne" ,last_name: "Johnson"}])
+puts "done seeding"
